a92ff1055c1168ba023336713b613eb97dd93cff
jihoon <akdlsz21@gmail.com>
2021-09-09 04:49:48 +0900

    Menu bar, extracting unique values(ES6). new Set(Array)

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import React, { useState } from 'react';
+import Menu from './Menu';
+import Categories from './Categories';
+import data from './data';
+
+const allCategories = [
+	'all',
+	...new Set(
+		data.map((item) => {
+			return item.category;
+		})
+	),
+];
+
+function App() {
+	const [menuItems, setMenuItems] = useState(data);
+	const [categories, setCategories] = useState(allCategories);
+
+	function filterItems(category) {
+		if (category === 'all') {
+			setMenuItems(data);
+			return;
+		}
+		const newItems = data.filter((item) => item.category === category);
+		setMenuItems(newItems);
+	}
+
+	return (
+		<main>
+			<section className="menu section">
+				<div className="title">
+					<h2>our menu</h2>
+					<div className="underline"></div>
+				</div>
+				<Categories filterItems={filterItems} categories={categories} />
+				<Menu items={menuItems} />
+			</section>
+		</main>
+	);
+}
+
+export default App;
